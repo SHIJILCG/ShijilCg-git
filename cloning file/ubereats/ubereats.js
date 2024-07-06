d907a3f40b18e9fb3f4e932e533a33be514c61f1
Shijil <shijilcg@gmail.com>
2024-07-06 08:44:20 +0530

    05-07-2024

--- cloning file/ubereats/ubereats.js
+++ cloning file/ubereats/ubereats.js
@@ -8,4 +8,20 @@ document.addEventListener('scroll',function(){
         addrboxtop.classList.remove('visible');
         addrboxtop.classList.add('hidden');
      }
-})
+     const headetoppartscroll=document.getElementById('heade-top-part-scroll');
+     if(window.scrollY>0){
+      headetoppartscroll.style.backgroundColor='white';
+     }else{
+      headetoppartscroll.style.backgroundColor='transparent';
+     }
+
+     const headetoppartaddrpart= document.querySelector('.find#heade-top-part-addr-part');
+     if(window.scrollY >100){
+        headetoppartaddrpart.classList.remove('hide');
+        headetoppartaddrpart.classList.add('show');
+     }else{
+        headetoppartaddrpart.classList.remove('show');
+        headetoppartaddrpart.classList.add('hide');
+     }
+});
+
